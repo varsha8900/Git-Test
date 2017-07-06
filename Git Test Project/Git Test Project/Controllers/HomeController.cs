cc9078464704289426bd252265794274262c600e
Varsha Agarwal <varsha890@gmail.com>
2017-07-06 11:44:55 -0700

    Removed the Contact View and Action

--- Git Test Project/Git Test Project/Controllers/HomeController.cs
+++ Git Test Project/Git Test Project/Controllers/HomeController.cs
@@ -19,12 +19,5 @@ namespace Git_Test_Project.Controllers
 
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
